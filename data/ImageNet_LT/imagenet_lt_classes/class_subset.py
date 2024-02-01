0c94f365f39b8c273f82bee9c0e94d45b2c0368b
JiahuiKChen <jiahui.k.chen@gmail.com>
2024-02-01 16:45:19 -0600

    files and code for 30-image subset

--- data/ImageNet_LT/imagenet_lt_classes/class_subset.py
+++ data/ImageNet_LT/imagenet_lt_classes/class_subset.py
@@ -4,30 +4,57 @@ import shutil
 
 # class labels for the classes wanted 
 classes = [
-    # 30 few classes: 
-    # first 10 are items or structures/scenes (includes hard cases like "iron, 606")
-    579, 606, 632, 702, 759, 718, 873, 866, 498, 776,
-    # next 10 are animals
-    397, 38, 15, 73, 102, 140, 142, 170, 260, 376,
-    # last 10 are food or clothes or abstract
-    568, 614, 916, 935, 959, 962, 963, 992, 738, 941,
+    # 10 few classes
+    # first 4 are items or structures/scenes (includes hard cases like "iron, 606")
+    # grand piano, iron, pier, "cinema, movie theater, movie theatre, movie house, picture palace"
+    579, 606, 718, 498, 
+    # next 3 are animals: pufferfish, "banded gecko", robin, 
+    397, 38, 15,
+    # last 3 are: fur coat, website, carbonara
+    568, 916, 959,
 
-    # 30 median classes
-    # first 10 are animals
-    359, 344, 377, 313, 273, 222, 63, 25, 6, 395,
-    # next 10 are items or structures/scenes 
-    750, 706, 703, 698, 690, 684, 838, 850, 919, 801,
-    # last 10 are food or clothes or people 
-    795, 983, 981, 982, 960, 697, 931, 928, 956, 967,
+    # 10 median classes
+    # first 4 are animals: hippo, marmoset, walking stick, dingo
+    344, 377, 313, 273, 
+    # next 3: quilt, park bench, "sunscreen, sunblock, sun blocker"
+    750, 703, 838,
+    # last 3: ski, scuba diver, "ballplayer, baseball player"
+    795, 983, 981,
 
-    # 30 many classes
-    # first 10 are animals
-    22, 29, 48, 108, 372, 388, 357, 394, 338, 341,
-    # next 10 are items or structures/scenes
-    404, 419, 460, 486, 540, 548, 567, 662, 712, 760, 
-    # last 10 are food or clothes 
-    443, 515, 578, 770, 961, 965, 655, 711, 903, 933
+    # 10 many classes
+    # first 3 are animals: "green snake, grass snake", sea anemone, llama
+    55, 108, 355,
+    # next 3 (items or structures/scenes): "breakwater, groin, groyne, mole, bulwark, seawall, jetty", cello, "drilling platform, offshore rig"  
+    460, 486, 540,
+    # last 4 are food or clothes: cowboy hat, gown, running shoe, burrito
+    515, 578, 770, 965
 ]
+# 30 classes each is still too many
+# classes = [
+#     # 30 few classes: 
+#     # first 10 are items or structures/scenes (includes hard cases like "iron, 606")
+#     579, 606, 632, 702, 759, 718, 873, 866, 498, 776,
+#     # next 10 are animals
+#     397, 38, 15, 73, 102, 140, 142, 170, 260, 376,
+#     # last 10 are food or clothes or abstract
+#     568, 614, 916, 935, 959, 962, 963, 992, 738, 941,
+
+#     # 30 median classes
+#     # first 10 are animals
+#     359, 344, 377, 313, 273, 222, 63, 25, 6, 395,
+#     # next 10 are items or structures/scenes 
+#     750, 706, 703, 698, 690, 684, 838, 850, 919, 801,
+#     # last 10 are food or clothes or people 
+#     795, 983, 981, 982, 960, 697, 931, 928, 956, 967,
+
+#     # 30 many classes
+#     # first 10 are animals
+#     22, 29, 48, 108, 372, 388, 357, 394, 338, 341,
+#     # next 10 are items or structures/scenes
+#     404, 419, 460, 486, 540, 548, 567, 662, 712, 760, 
+#     # last 10 are food or clothes 
+#     443, 515, 578, 770, 961, 965, 655, 711, 903, 933
+# ]
 class_set = set(classes)
 
 # generate train, val, and test txt files for given classes
@@ -37,9 +64,9 @@ data_files = [
     "/mnt/zhang-nas/jiahuic/LT-classification/data/ImageNet_LT/ImageNet_LT_test.txt"
     ]
 out_files = [
-    "/mnt/zhang-nas/jiahuic/LT-classification/data/ImageNet_LT/ImageNet_LT_train_90.txt",
-    "/mnt/zhang-nas/jiahuic/LT-classification/data/ImageNet_LT/ImageNet_LT_val_90.txt",
-    "/mnt/zhang-nas/jiahuic/LT-classification/data/ImageNet_LT/ImageNet_LT_test_90.txt" 
+    "/mnt/zhang-nas/jiahuic/LT-classification/data/ImageNet_LT/ImageNet_LT_train_30.txt",
+    "/mnt/zhang-nas/jiahuic/LT-classification/data/ImageNet_LT/ImageNet_LT_val_30.txt",
+    "/mnt/zhang-nas/jiahuic/LT-classification/data/ImageNet_LT/ImageNet_LT_test_30.txt" 
     ]
 for i in range(len(data_files)):
     output_str = ""
@@ -52,8 +79,8 @@ for i in range(len(data_files)):
             if class_label in class_set:
                 output_str += line
 
-    # with open(out_file, "w") as outuput_file:
-    #     outuput_file.write(output_str)
+    with open(out_file, "w") as outuput_file:
+        outuput_file.write(output_str)
 
 # move generated images of these classes 
 source_dirs = [
@@ -62,9 +89,9 @@ source_dirs = [
     "/mnt/zhang-nas/jiahuic/synth_LT_data/ImageNetLT/mixup",
 ] # TODO: dropout once it's done 
 destination_dirs = [
-    "/mnt/zhang-nas/jiahuic/synth_LT_data/ImageNetLT/rand_img_cond_90",
-    "/mnt/zhang-nas/jiahuic/synth_LT_data/ImageNetLT/cutmix_90", 
-    "/mnt/zhang-nas/jiahuic/synth_LT_data/ImageNetLT/mixup_90",
+    "/mnt/zhang-nas/jiahuic/synth_LT_data/ImageNetLT/rand_img_cond_30",
+    "/mnt/zhang-nas/jiahuic/synth_LT_data/ImageNetLT/cutmix_30", 
+    "/mnt/zhang-nas/jiahuic/synth_LT_data/ImageNetLT/mixup_30",
 ]
 for i in range(len(source_dirs)):
     source_dir = source_dirs[i]
@@ -72,5 +99,5 @@ for i in range(len(source_dirs)):
     all_imgs = listdir(source_dir)
     for img in all_imgs:
         label = int(img.split("_")[0])
-        # if label in class_set:
-            # shutil.copy(f"{source_dir}/{img}", f"{dest_dir}/{img}")
+        if label in class_set:
+            shutil.copy(f"{source_dir}/{img}", f"{dest_dir}/{img}")
