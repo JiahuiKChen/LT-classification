c2528d485524433de1fa633c8341958ef4a59855
JiahuiKChen <jiahui.k.chen@gmail.com>
2024-03-19 13:18:00 -0500

    Running FULL SIZE dropout, embed-cutmix / embed-mixup with dorpout on 30 subset

--- main.py
+++ main.py
@@ -79,10 +79,10 @@ with open(args.cfg) as f:
 config = update(config, args)
 
 wandb.init(
-    mode="disabled",
+    # mode="disabled",
     project="ImageNet-LT",
     config=config,
-    name="cutmix",
+    name="embed_cutmix_dropout_fix",
     group="30subset_resnext18_BS"
 )
 
