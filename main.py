d2e3fc722433cacd7b0c919c4cf55a0cd41b1933
JiahuiKChen <jiahui.k.chen@gmail.com>
2024-05-08 14:47:41 -0500

    dropout ready for training on A100

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ wandb.init(
     # mode="disabled",
     project="ImageNet-LT",
     config=config,
-    name="cutmix_dropout_resnext50_e104start_A100",
+    name="dropout_resnext50_e115start_A100",
     # group="test",
 )
 
